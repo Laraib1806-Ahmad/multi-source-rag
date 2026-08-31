a835ee996d66c9639e50ed9e0341a71373e33ee1
Laraib Ahmad <laraibsheikh1806@gmail.com>
2026-08-31 11:44:22 +0500

    sample

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("hello");
+import { createAgent, tool } from "langchain";
+import * as z from "zod";
+
+const getWeather = tool((input) => `It's always sunny in ${input.city}!`, {
+  name: "get_weather",
+  description: "Get the weather for a given city",
+  schema: z.object({
+    city: z.string().describe("The city to get the weather for"),
+  }),
+});
+
+const agent = createAgent({
+  model: "llama3.2:latest",
+  tools: [getWeather],
+});
+
+console.log(
+  await agent.invoke({
+    messages: [
+      { role: "user", content: "What's the weather in San Francisco?" },
+    ],
+  }),
+);
